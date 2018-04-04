d8d64a6437a65f0beb473b903c1dde9f66221351
Hanseltu <tuhaoxin6@gmail.com>
2018-04-04 22:57:06 +0800

    master

---
+++ 7.ReverseInteger/reverseInteger.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+
+#define INT_MAX pow(2,31)
+#define INT_MIN -pow(2,31)
+int reverse(int);
+
+//function
+//Call the codes NICE!
+int reverse(int x){
+    long long val = 0;
+    do{
+        val = val * 10 + x % 10;
+        x /= 10;
+    }
+    while(x);
+    return (val > INT_MAX || val < INT_MIN) ? 0 : val;
+}
+
+int main(){
+    int temp;
+    printf("Please input an integer under 32-bit \n");
+    scanf("%d",&temp);
+    printf("The integer reversed is %d \n",reverse(temp));
+}
