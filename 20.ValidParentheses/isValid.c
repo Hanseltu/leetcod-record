1ff28661f2793c503b856199b51b8b466a286057
Hanseltu <tuhaoxin6@gmail.com>
2018-04-12 23:02:08 +0800

    master

--- 20.ValidParentheses/isValid.c
+++ 20.ValidParentheses/isValid.c
@@ -1,4 +1,46 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
+bool isValid(char *s){
+    int len = strlen(s);
+    if (len%2 != 0)
+    {
+        return false;
+    }
 
+    int limit = len / 2;
+    char *stack = malloc(limit);
+    int idx = 0;
+    char cur,peek;
+    for (int i = 0;i < len; i++)
+    {
+        cur = s[i];
+        if (cur == 'c' || cur == '[' || cur == '{')
+        {
+            if (idx == limit)
+            {
+                return false;
+            }
+            else
+            {
+                stack[idx++] = cur;
+            }
+        }
+        else
+        {
+            if (idx == 0)
+            {
+                return false;
+            }
+            peek = stack[idx-1];
+            if ((peek == '(' && cur == ')')
+                    || (peek == '[' && cur == ']')
+                    || (peek == '{' && cur == '}'))
+            {
+                idx --;
+            }
+        }
+    }
+    return idx == 0;
+}
