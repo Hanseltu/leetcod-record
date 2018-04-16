33625679e2fc9c5e23f0e2a1e49986884e093cb3
thx <tuhaoxin6@gmail.com>
2018-04-16 22:45:30 +0800

    Update removeElement.c

--- 27.RemoveElement/removeElement.c
+++ 27.RemoveElement/removeElement.c
@@ -1,1 +1,15 @@
 #include<stdio.h>
+
+int removeElement(int* nums, int numsSize, int val) {
+  int i,j;
+  for(i=0,j=0;i<numsSize;i++)
+  {
+    if(nums[i] != val)
+    {
+      nums[j++] = nums[i];
+      //numsSize--;
+    }
+   
+  }
+  return (numsSize>0?j+1:j);
+}
