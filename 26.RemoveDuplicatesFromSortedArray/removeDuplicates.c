cb4140ca989efe1803134f36ea2d63a9e4b8b0e0
Hanseltu <tuhaoxin6@gmail.com>
2018-04-16 21:47:29 +0800

    master

--- 26.RemoveDuplicatesFromSortedArray/removeDuplicates.c
+++ 26.RemoveDuplicatesFromSortedArray/removeDuplicates.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
-int removeDuplicates(int* nums, int numsSize) {
-    for(int i=0;i<numsSize;){
-      if (nums[i] == nums[i+1] )
+//超出时间限制
+int removeDuplicates1(int* nums, int numsSize) {
+    for(int i=1;i<numsSize;){
+      if (nums[i] == nums[i-1] )
       {
-        for(int j = i+1; j < numsSize;j++)
+        for(int j = i; j < numsSize;j++)
         {
           nums[j] = nums[j+1];
-        }      
+        }
         numsSize--;
       }
       if (nums[i] != nums[i+1])
@@ -15,9 +16,19 @@ int removeDuplicates(int* nums, int numsSize) {
   }
   return numsSize;
 }
+
+
+
+//AC
 int removeDuplicates(int* nums, int numsSize) {
     int i, j;
-    for (i = 1, j = 0; i < numsSize; i++) 
+    for (i = 1, j = 0; i < numsSize; i++)
         if (nums[i] != nums[i-1]) { nums[++j] = nums[i]; }
     return (numsSize > 0 ? j+1 : j);
+}
+
+int main(){
+    int a[] = {0,0,0,0};
+    printf ("%d \n",removeDuplicates1(a,sizeof(a)));
+    return 0;
 }
