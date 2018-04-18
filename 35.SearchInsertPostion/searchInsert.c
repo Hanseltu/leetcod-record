90fde54d9eb7ec1fa7b3d9e4898d886f70d4c8cd
thx <tuhaoxin6@gmail.com>
2018-04-18 14:47:58 +0800

    Update searchInsert.c

--- 35.SearchInsertPostion/searchInsert.c
+++ 35.SearchInsertPostion/searchInsert.c
@@ -1,3 +1,12 @@
 #include<stdio.h>
 
-
+int searchInsert(int* nums, int numsSize, int target) {
+  for (int i = 0;i < numsSize;i++)
+  {
+    if (nums[i] > target || nums[i] == target)
+        return i;
+    if( (i == numsSize -1) || (nums[i] < target && nums[i+1] > target) )
+        return i+1;
+}
+  return 0;
+}
