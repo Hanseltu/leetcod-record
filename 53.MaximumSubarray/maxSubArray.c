2da572c417be21dd643c71557e92ced293283826
thx <tuhaoxin6@gmail.com>
2018-04-22 23:24:40 +0800

    Update maxSubArray.c

--- 53.MaximumSubarray/maxSubArray.c
+++ 53.MaximumSubarray/maxSubArray.c
@@ -1,1 +1,18 @@
 #include<stdio.h>
+
+int MaxSubSeqSum(int arrays[],int length){
+    int i,j,k,thisSum=0,maxSum=0;
+    for(i=0;i<length;i++){
+        for(j=i;j<length;j++){
+            thisSum=0;
+            for(k=i;k<=j;k++){
+                thisSum+=arrays[k];
+            }
+            if(thisSum>maxSum)
+              maxSum=thisSum;
+        };
+    }
+    return maxSum;
+}
+
+
