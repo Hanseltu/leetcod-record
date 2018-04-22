437fe431d473da2175d8492e51d320fbe4eea6ee
thx <tuhaoxin6@gmail.com>
2018-04-22 23:25:42 +0800

    Update maxSubArray.c

--- 53.MaximumSubarray/maxSubArray.c
+++ 53.MaximumSubarray/maxSubArray.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 
+//O(n^3)
 int MaxSubSeqSum(int arrays[],int length){
     int i,j,k,thisSum=0,maxSum=0;
     for(i=0;i<length;i++){
@@ -15,4 +16,16 @@ int MaxSubSeqSum(int arrays[],int length){
     return maxSum;
 }
 
+//改进O(n^2)
+int MaxSubSeqSum(int arrays[],int length){
+    int i,j,thisSum=0,maxSum=0;
+    for(i=0;i<length;i++){//i是子列左端
+        thisSum=0;//从arrays[i]到arrays[j]的子序列和
+        for(j=i;j<length;j++){//j是子序列右端
+            thisSum+=arrays[j];
+            if(thisSum>maxSum)maxSum=thisSum;//如果本次求和大于最终结果，更新maxSum
+        };
+    }
+    return maxSum;
+}
 
