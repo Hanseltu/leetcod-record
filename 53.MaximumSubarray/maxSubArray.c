090b701caa4300ba7b66e59c8dd4ad8a63ca2c3c
thx <tuhaoxin6@gmail.com>
2018-04-22 23:27:12 +0800

    Update maxSubArray.c

--- 53.MaximumSubarray/maxSubArray.c
+++ 53.MaximumSubarray/maxSubArray.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 //O(n^3)
-int MaxSubSeqSum(int arrays[],int length){
+int MaxSubArray(int arrays[],int length){
     int i,j,k,thisSum=0,maxSum=0;
     for(i=0;i<length;i++){
         for(j=i;j<length;j++){
@@ -28,4 +28,39 @@ int MaxSubSeqSum(int arrays[],int length){
     }
     return maxSum;
 }
+
+
+
+//分治法
+int MaxSubArray(int arrays[], int left, int right) {
+    int sum = 0;
+    if (left == right) {
+        if (arrays[left] > 0)return arrays[left];
+        else sum = 0;
+    } else {
+        int middle = (left + right) / 2;
+        int leftSum = MaxSubSeqSum(arrays, left, middle);
+        int rightSum = MaxSubSeqSum(arrays, middle + 1, right);
+        int finalLeftSum = 0, thisLeftSum = 0;
+        for (int i = left; i <=middle; i++) {
+            thisLeftSum += arrays[i];
+            if (thisLeftSum > finalLeftSum)
+                finalLeftSum = thisLeftSum;
+        }
+        int finalRightSum = 0, thisRightSum = 0;
+        for (int j = middle + 1; j < right; j++) {
+            thisRightSum += arrays[j];
+            if (thisRightSum > finalRightSum)
+                finalRightSum = thisRightSum;
+        }
+        sum = finalLeftSum + finalRightSum;
+        printf("left sum is %d,right sum is %d\n",finalLeftSum,finalRightSum);
+        if (sum < leftSum)
+            sum = leftSum;
+        if (sum < rightSum)
+            sum = rightSum;
+    }
+    return sum;
+}
+
 
