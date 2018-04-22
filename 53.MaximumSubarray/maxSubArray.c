bd58cb8cecebdb89731e182969b548d441e74639
Hanseltu <tuhaoxin6@gmail.com>
2018-04-22 23:27:02 +0800

    master

--- 53.MaximumSubarray/maxSubArray.c
+++ 53.MaximumSubarray/maxSubArray.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 //O(n^3)
-int MaxSubArray(int arrays[],int length){
+int maxSubArray1(int arrays[],int length){
     int i,j,k,thisSum=0,maxSum=0;
     for(i=0;i<length;i++){
         for(j=i;j<length;j++){
@@ -17,7 +17,7 @@ int MaxSubArray(int arrays[],int length){
 }
 
 //改进O(n^2)
-int MaxSubSeqSum(int arrays[],int length){
+int maxSubArray2(int arrays[],int length){
     int i,j,thisSum=0,maxSum=0;
     for(i=0;i<length;i++){//i是子列左端
         thisSum=0;//从arrays[i]到arrays[j]的子序列和
@@ -32,15 +32,15 @@ int MaxSubSeqSum(int arrays[],int length){
 
 
 //分治法
-int MaxSubArray(int arrays[], int left, int right) {
+int maxSubArray3(int arrays[], int left, int right) {
     int sum = 0;
     if (left == right) {
         if (arrays[left] > 0)return arrays[left];
         else sum = 0;
     } else {
         int middle = (left + right) / 2;
-        int leftSum = MaxSubSeqSum(arrays, left, middle);
-        int rightSum = MaxSubSeqSum(arrays, middle + 1, right);
+        int leftSum = maxSubArray3(arrays, left, middle);
+        int rightSum = maxSubArray3(arrays, middle + 1, right);
         int finalLeftSum = 0, thisLeftSum = 0;
         for (int i = left; i <=middle; i++) {
             thisLeftSum += arrays[i];
