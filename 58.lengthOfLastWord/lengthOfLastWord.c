2356b41486ee0fa532361995541cb8637f866896
thx <tuhaoxin6@gmail.com>
2018-05-06 00:18:50 +0800

    Update lengthOfLastWord.c

--- 58.lengthOfLastWord/lengthOfLastWord.c
+++ 58.lengthOfLastWord/lengthOfLastWord.c
@@ -1,14 +1,18 @@
-// 有问题
 #include<stdio.h>
 #include<string.h>
 int lengthOfLastWord(char* s) {
   int len = strlen(s);
-  int count = 0;
-  for (int i = len; i > 0; i--)
+  int count = 0,a = 0;
+  while(s[len-1] == ' ')
+  {
+    len --;
+    continue;
+  }
+  for (int i = len; i > 0 ; i--)
   {
     if (s[i-1] == ' ')
       break;
-    count++;
+    count ++;
   }
   return count;
 }
