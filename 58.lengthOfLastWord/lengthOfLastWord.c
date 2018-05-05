b50de50a958a848610c8306784304050c15d126a
thx <tuhaoxin6@gmail.com>
2018-05-05 22:27:52 +0800

    Update lengthOfLastWord.c

--- 58.lengthOfLastWord/lengthOfLastWord.c
+++ 58.lengthOfLastWord/lengthOfLastWord.c
@@ -1,1 +1,13 @@
 #include<stdio.h>
+
+int lengthOfLastWord(char* s) {
+  int len = sizeof(s);
+  int count;
+  for (int i = len; i > 0; i--)
+  {
+    if (s[i-1] = ' ')
+      break;
+    count ++;
+  }
+  return count;
+}
